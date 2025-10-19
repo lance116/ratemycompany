4a54e97e3d491c83ca69974ea104d03808814526
lance116 <langyan00716@gmail.com>
2025-10-19 09:47:44 -0400

    callback

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Vote from "./pages/Vote";
 import Leaderboard from "./pages/Leaderboard";
@@ -27,6 +27,7 @@ const App = () => {
               <Route path="/leaderboard" element={<Leaderboard />} />
               <Route path="/reviews" element={<Reviews />} />
               <Route path="/company/:id" element={<CompanyDetails />} />
+              <Route path="/auth/callback" element={<Navigate to="/" replace />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
